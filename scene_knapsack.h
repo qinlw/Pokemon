565b9090af01132d3dd26e24d972ecc18015a27b
2162036628@qq.com <2162036628@qq.com>
2024-10-09 11:01:28 +0800

    新增了背包背景界面及返回逻辑

--- scene_knapsack.h
+++ scene_knapsack.h
@@ -3,10 +3,50 @@
 #include "scene.h"
 
 
+const int lattice_num = 25;
+
 class SceneKnapsack : public Scene {
 public:
 	void on_enter() {
 		std::cout << "knapsack";
+
+		// 背景位置
+		int knapsack_field_background_to_left_screen_distance = 25, knapsack_field_background_to_top_screen_distance = 125;
+		int player_id_offset_x = 30, player_id_offset_y = 13;
+		int knapsack_lattice_to_knapsack_field_background_distance = 20;
+		knapsack_field_background_1P_pos.x = background_base_pos.x + knapsack_field_background_to_left_screen_distance;
+		knapsack_field_background_1P_pos.y = background_base_pos.y + knapsack_field_background_to_top_screen_distance;
+		knapsack_field_background_2P_pos.x = getwidth() / 2 + img_knapsack_field_background.getwidth() - knapsack_field_background_to_left_screen_distance;
+		knapsack_field_background_2P_pos.y = knapsack_field_background_1P_pos.y;
+		player_1P_pos.x = knapsack_field_background_1P_pos.x + player_id_offset_x;
+		player_1P_pos.y = knapsack_field_background_1P_pos.y + player_id_offset_y;
+		player_2P_pos.x = knapsack_field_background_2P_pos.x + player_id_offset_x;
+		player_2P_pos.y = knapsack_field_background_2P_pos.y + player_id_offset_y;
+		knapsack_1P_pos.x = player_1P_pos.x + img_player_1P.getwidth();
+		knapsack_1P_pos.y = player_1P_pos.y;
+		knapsack_2P_pos.x = player_2P_pos.x + img_player_2P.getwidth();
+		knapsack_2P_pos.y = knapsack_1P_pos.y;
+		knapsack_lattice_1P_pos[0].x = player_1P_pos.x + img_player_1P.getwidth() / 2;
+		knapsack_lattice_1P_pos[0].y = knapsack_field_background_1P_pos.y + img_knapsack_field_background.getheight()
+			+ knapsack_lattice_to_knapsack_field_background_distance;
+		knapsack_lattice_2P_pos[0].x = player_2P_pos.x + img_player_2P.getwidth() / 2;
+		knapsack_lattice_2P_pos[0].y = knapsack_field_background_2P_pos.y + img_knapsack_field_background.getheight()
+			+ knapsack_lattice_to_knapsack_field_background_distance;
+		for (int i = 1; i < lattice_num; ++i) {
+			knapsack_lattice_1P_pos[i].x = knapsack_lattice_1P_pos[i - 1].x + img_knapsack_lattice.getwidth();
+			knapsack_lattice_1P_pos[i].y = knapsack_lattice_1P_pos[i - 1].y;
+			knapsack_lattice_2P_pos[i].x = knapsack_lattice_2P_pos[i - 1].x + img_knapsack_lattice.getwidth();
+			knapsack_lattice_2P_pos[i].y = knapsack_lattice_2P_pos[i - 1].y;
+			if (i % 5 == 0) {
+				knapsack_lattice_1P_pos[i].x = knapsack_lattice_1P_pos[0].x;
+				knapsack_lattice_1P_pos[i].y += img_knapsack_lattice.getheight();
+				knapsack_lattice_2P_pos[i].x = knapsack_lattice_2P_pos[0].x;
+				knapsack_lattice_2P_pos[i].y += img_knapsack_lattice.getheight();
+			}
+		}
+
+		return_btn_pos.x = getwidth() - img_return_button.getwidth() - 25;
+		return_btn_pos.y = getheight() - img_return_button.getheight() - 20;
 	}
 
 	void on_update(int delta) {
@@ -14,15 +54,76 @@ public:
 	}
 
 	void on_draw() {
+		// 背景
+		putimage_alpha(background_base_pos.x, background_base_pos.y, &img_background_base);
+		putimage_alpha(knapsack_field_background_1P_pos.x, knapsack_field_background_1P_pos.y, &img_knapsack_field_background);
+		putimage_alpha(knapsack_field_background_2P_pos.x, knapsack_field_background_2P_pos.y, &img_knapsack_field_background);
+		putimage_alpha(player_1P_pos.x, player_1P_pos.y, &img_player_1P);
+		putimage_alpha(player_2P_pos.x, player_2P_pos.y, &img_player_2P);
+		putimage_alpha(knapsack_1P_pos.x, knapsack_1P_pos.y, &img_knapsack);
+		putimage_alpha(knapsack_2P_pos.x, knapsack_2P_pos.y, &img_knapsack);
+		for (int i = 0; i < lattice_num; ++i) {
+			putimage_alpha(knapsack_lattice_1P_pos[i].x, knapsack_lattice_1P_pos[i].y, &img_knapsack_lattice);
+			putimage_alpha(knapsack_lattice_2P_pos[i].x, knapsack_lattice_2P_pos[i].y, &img_knapsack_lattice);
+		}
 
+		putimage_alpha(return_btn_pos.x, return_btn_pos.y, &img_return_button);
 	}
 
 	void on_input(const ExMessage& msg) {
-
+		switch (msg.message) {
+			// 鼠标左键按下
+		case WM_LBUTTONDOWN: {
+			// 返回按钮
+			if (msg.x >= return_btn_pos.x && msg.x <= return_btn_pos.x + img_return_button.getwidth() &&
+				msg.y >= return_btn_pos.y && msg.y <= return_btn_pos.y + img_return_button.getheight()) {
+				is_return_btn = true;
+				if (is_open_sound_effect) mciSendString(_T("play click_sound_2_1 from 0"), NULL, 0, NULL);
+				button_sink_animatioin(5, 50, return_btn_pos, &img_return_button);
+				break;
+			}
+		}
+			// 鼠标左键弹起
+		case WM_LBUTTONUP: {
+			// 返回按钮
+			if (is_return_btn) {
+				if (msg.x >= return_btn_pos.x && msg.x <= return_btn_pos.x + img_return_button.getwidth() &&
+					msg.y >= return_btn_pos.y && msg.y <= return_btn_pos.y + img_return_button.getheight()) {
+					is_return_btn = false;
+					if (is_open_sound_effect) mciSendString(_T("play click_sound_2_2 from 0"), NULL, 0, NULL);
+					button_bulge_animatioin(5, 300, return_btn_pos, &img_return_button);
+					scene_manager->switch_scene(SceneManager::SceneType::Menu);
+				}
+				else {
+					if (is_return_btn) {
+						is_return_btn = false;
+						if (is_open_sound_effect) mciSendString(_T("play click_sound_2_2 from 0"), NULL, 0, NULL);
+						button_bulge_animatioin(5, 300, return_btn_pos, &img_return_button);
+					}
+				}
+			}
+		}
+		}
 	}
 
 	void on_exit() {
 
 	}
+
+
+private:
+	POINT background_base_pos = { 0 };
+	POINT knapsack_field_background_1P_pos = { 0 };
+	POINT knapsack_field_background_2P_pos = { 0 };
+	POINT player_1P_pos = { 0 };
+	POINT player_2P_pos = { 0 };
+	POINT knapsack_1P_pos = { 0 };
+	POINT knapsack_2P_pos = { 0 };
+	POINT knapsack_lattice_1P_pos[lattice_num] = { 0 };
+	POINT knapsack_lattice_2P_pos[lattice_num] = { 0 };
+	POINT return_btn_pos = { 0 };
+
+
+	bool is_return_btn = false;													// 返回按钮是否被按下
 
 };
